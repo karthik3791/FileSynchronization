8f32ac015cecb9762e4a707530966c964c5270ec
Karthik <karthik3791@gmail.com>
2014-04-05 23:58:18 +0800

    Master : PeerInstance.java

--- src/com/filesync/peer/PeerClient.java
+++ src/com/filesync/peer/PeerClient.java
@@ -22,189 +22,194 @@ import com.filesync.shareddir.SharedFolderDetails;
 
 public class PeerClient implements Runnable {
 
-
 	static Logger logger = Logger.getLogger(PeerClient.class);
 
-	public boolean keep_alive ;
+	public boolean keep_alive;
 	boolean status = true;
 	private Socket socket;
 	private FileOutputStream fos;
 	private boolean file_write = false;
 	private String file_being_pulled;
 	private File dwfile;
-	private String remote_ip;	
-	private Map<String, PeerClient> active_download_connections;
+	private final String remote_ip;
+	private final Map<String, PeerClient> active_download_connections;
 
 	/*
 	 * Logging using Apache log4j
 	 */
-	public void logMessage(String s) {		
+	public void logMessage(String s) {
 
-		logger.info(s);		
+		logger.info(s);
 	}
 
-	public PeerClient(String remoteip, Map<String,PeerClient>map){
-		remote_ip =remoteip;
+	public PeerClient(String remoteip, Map<String, PeerClient> map) {
+		remote_ip = remoteip;
 		active_download_connections = map;
 		keep_alive = true;
 
-		//Initializing logging file.
-		SimpleLayout layout = new SimpleLayout();    
+		// Initializing logging file.
+		SimpleLayout layout = new SimpleLayout();
 		FileAppender appender;
 		try {
-			appender = new FileAppender(layout,SharedFolderDetails.logdir+File.separatorChar+"Thread_downloading_from_"+remote_ip+"_log",false);
+			appender = new FileAppender(layout, SharedFolderDetails.logdir
+					+ File.separatorChar + "Thread_downloading_from_"
+					+ remote_ip + "_log", false);
 			logger.addAppender(appender);
-			logger.setLevel((Level) Level.DEBUG);
+			logger.setLevel(Level.DEBUG);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}    
+		}
 	}
-
 
 	@Override
 	public void run() {
 
-		File syncdir = new File(SharedFolderDetails.directorypath);	
+		File syncdir = new File(SharedFolderDetails.directorypath);
 
 		// Try to connect to the remote host.
 		try {
-			socket = new Socket(remote_ip,5000);
-			//1. Set the timeout duration for the socket.	
+			socket = new Socket(remote_ip, 5000);
+			// 1. Set the timeout duration for the socket.
 			this.socket.setSoTimeout(4000);
 		} catch (UnknownHostException e1) {
-			logMessage("Could not make a download connection to "+remote_ip);
+			logMessage("Could not make a download connection to " + remote_ip);
 			logMessage("Got an Unknown Host Exception. Check if node is up and running.");
-			active_download_connections.remove(remote_ip);		
+			active_download_connections.remove(remote_ip);
 			status = false;
 
 		} catch (IOException e1) {
-			logMessage("Could not make a download connection to "+remote_ip);
+			logMessage("Could not make a download connection to " + remote_ip);
 			logMessage("Got an IOException. Check if node is up and running.");
 			active_download_connections.remove(remote_ip);
 			status = false;
 		}
 
-		try{					
+		try {
 
+			while (status) {
 
-			while(status){
-				
-				if(keep_alive==false){
+				if (keep_alive == false) {
 					logMessage("Force close connection of client by user. Closing connection.");
 					close_connection();
 					break;
 				}
 
-				//--PeerClient Ready-------------//
+				// --PeerClient Ready-------------//
 				logMessage("Download Thread ready");
-				//2. Send Pull Request		
-				send_over_socket("PULL",null);
+				// 2. Send Pull Request
+				send_over_socket("PULL", null);
 				logMessage("Sent Pull Request");
-				//3.Get List of Files in Remote node's shared folder.
-				Packet file_list_pkt = read_from_socket();		
-				if(file_list_pkt.getflag().equals("FILELIST")){
+				// 3.Get List of Files in Remote node's shared folder.
+				Packet file_list_pkt = read_from_socket();
+				if (file_list_pkt.getflag().equals("FILELIST")) {
 
 					String[] remote_file_list = file_list_pkt.getControlData();
 					String[] local_file_list = getSyncDirFiles(syncdir);
 
-					//4. Compare the local file list with the remote file list. Filter the files that needs to be pulled.
-					String[] pull_list = getRequiredFiles(remote_file_list,local_file_list);
+					// 4. Compare the local file list with the remote file list.
+					// Filter the files that needs to be pulled.
+					String[] pull_list = getRequiredFiles(remote_file_list,
+							local_file_list);
 
-					//5. If pull list is null
-					if(pull_list == null){
-						//5.1 Inform Server nothing to pull as of this moment.
-						send_over_socket("NOPULL",null);
+					// 5. If pull list is null
+					if (pull_list == null) {
+						// 5.1 Inform Server nothing to pull as of this moment.
+						send_over_socket("NOPULL", null);
 						logMessage("NO PULL");
-					}
-					else{
-						//5. Send the list of files which you need to pull from the node.
-						send_over_socket("PULLLIST",pull_list);
+					} else {
+						// 5. Send the list of files which you need to pull from
+						// the node.
+						send_over_socket("PULLLIST", pull_list);
 						logMessage("Required Files by client ");
-						for(String fname: pull_list){
+						for (String fname : pull_list) {
 							logMessage(fname);
-						}						
-						//6. Check if no need for manual closing of connection.
-						if(keep_alive==true){
-							//6.1 Send OK to server.
-							send_over_socket("PULLREADY",null);
+						}
+						// 6. Check if no need for manual closing of connection.
+						if (keep_alive == true) {
+							// 6.1 Send OK to server.
+							send_over_socket("PULLREADY", null);
 
-							//7 Read Server status 
+							// 7 Read Server status
 							Packet server_status = read_from_socket();
-							if(server_status.getflag().equals("PUSHREADY")){
-								//Both server and client are ready to exchange file.
+							if (server_status.getflag().equals("PUSHREADY")) {
+								// Both server and client are ready to exchange
+								// file.
 
-								//8. For each file in pull_list
-								for(String pull_file: pull_list){
+								// 8. For each file in pull_list
+								for (String pull_file : pull_list) {
 
-									//8.1 Send Pull filename
-									send_over_socket("PULLFILE",new String[]{pull_file});				
-									logMessage("PULL : "+pull_file);
-									//8.2 Open the new file to write.
-									String filepath = SharedFolderDetails.directorypath+File.separatorChar+pull_file;
+									// 8.1 Send Pull filename
+									send_over_socket("PULLFILE",
+											new String[] { pull_file });
+									logMessage("PULL : " + pull_file);
+									// 8.2 Open the new file to write.
+									String filepath = SharedFolderDetails.directorypath
+											+ File.separatorChar + pull_file;
 									this.file_being_pulled = filepath;
 									dwfile = new File(filepath);
 									fos = new FileOutputStream(dwfile);
 									file_write = true;
-									while(true){
-										// 8.3 Read File Packets. (Packet with flag as FILE	)
-										Packet filepkt = read_from_socket();	
-										if(filepkt.getflag().equals("FILE")){
-											logMessage(pull_file+": Read a chunk");
-											fos.write(filepkt.getFileData());											
+									while (true) {
+										// 8.3 Read File Packets. (Packet with
+										// flag as FILE )
+										Packet filepkt = read_from_socket();
+										if (filepkt.getflag().equals("FILE")) {
+											logMessage(pull_file
+													+ ": Read a chunk");
+											fos.write(filepkt.getFileData());
 
-										}else if(filepkt.getflag().equals("FILECOMPLETE")){
+										} else if (filepkt.getflag().equals(
+												"FILECOMPLETE")) {
 											file_write = false;
 											fos.close();
-											logMessage(pull_file+" has been downloaded successfully");
+											logMessage(pull_file
+													+ " has been downloaded successfully");
 											break;
 										}
-										
-										if(keep_alive==false){
+
+										if (keep_alive == false) {
 											logMessage("Was asked to terminate connection immediately. Terminating connection.");
 											close_connection();
 										}
 
-									}		
+									}
 								}
 
 								logMessage("Downloaded all files successfully");
 
-							}
-							else if (server_status.getflag().equals("QUIT")){
-								//Server wants to close connection
+							} else if (server_status.getflag().equals("QUIT")) {
+								// Server wants to close connection
 								logMessage("Node's server thread quit before file sync. So closing connection.");
 								close_connection();
 							}
-						}else{
-							send_over_socket("QUIT",null);
+						} else {
+							send_over_socket("QUIT", null);
 							logMessage("Was asked to close connection. File sync hasnt begun yet.");
 							// Client wants to close connection.
 							close_connection();
 						}
 					}
-				}
-				else{
+				} else {
 					// If it was not FileList
 				}
 				logMessage("Going to sleep for 1 minute");
 				Thread.sleep(60000);
 			}
 
-		}catch (SocketTimeoutException e){
+		} catch (SocketTimeoutException e) {
 			logMessage("Timeout occured while reading from the socket.");
 			logMessage("Plausible reason: Node is down.");
 			logMessage("Closing the socket.");
-			close_connection();			
+			close_connection();
 
-		}catch(IOException ie){
+		} catch (IOException ie) {
 			logMessage("Connection lost to the node. Closing the socket.");
-			close_connection();						
+			close_connection();
 
 		} catch (ClassNotFoundException ce) {
 			logMessage("Class Not found Exception caught. Closing the connection.");
-			close_connection();			
-
+			close_connection();
 
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
@@ -213,30 +218,32 @@ public class PeerClient implements Runnable {
 
 		}
 
-
 	}
 
-	//Function that will close the connection.
-	public void close_connection(){
-		// Remove the connection tracked in active_upload_connections and close the socket.
+	// Function that will close the connection.
+	public void close_connection() {
+		// Remove the connection tracked in active_upload_connections and close
+		// the socket.
 
-		active_download_connections.remove(remote_ip);		
+		active_download_connections.remove(remote_ip);
 		logMessage("Removed connection from hashmap tracking active download connections. Now closing socket...");
 
-		if(file_write == true){
-			// Chance that the file was being written halfway when connection was lost.
+		if (file_write == true) {
+			// Chance that the file was being written halfway when connection
+			// was lost.
 			try {
 				fos.flush();
 				fos.close();
 				File tfile = new File(file_being_pulled);
-				if(tfile.exists()){
-					logMessage("File which was being pulled when connection was lost, is now being deleted. Status: "+tfile.delete());
+				if (tfile.exists()) {
+					logMessage("File which was being pulled when connection was lost, is now being deleted. Status: "
+							+ tfile.delete());
 				}
 				logMessage("Closed FileOutputStream.");
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				logMessage("FileOutputStream was already closed");
-			}catch(NullPointerException ne){
+			} catch (NullPointerException ne) {
 				logMessage("FileOutputStream was not instantiated.");
 			}
 		}
@@ -251,75 +258,72 @@ public class PeerClient implements Runnable {
 
 	}
 
-
-
-
-
-	//-- Compares local_file_list against remote_file_list and filters only the required files --//
+	// -- Compares local_file_list against remote_file_list and filters only the
+	// required files --//
 	private String[] getRequiredFiles(String[] remote_file_list,
-			String[] local_file_list) {		
+			String[] local_file_list) {
 
 		Vector<String> needed_files = new Vector<String>();
 
-		for(String remotefile: remote_file_list){
-			if(Arrays.asList(local_file_list).contains(remotefile) == false){
+		for (String remotefile : remote_file_list) {
+			if (Arrays.asList(local_file_list).contains(remotefile) == false) {
 				needed_files.add(remotefile);
 			}
 		}
 
-		if(needed_files.size()<1){
+		if (needed_files.size() < 1) {
 			return null;
 		}
 
 		String[] filtered_files = new String[needed_files.size()];
 
-		for(int i=0;i<needed_files.size();i++){
+		for (int i = 0; i < needed_files.size(); i++) {
 			filtered_files[i] = needed_files.get(i);
 		}
 
 		return filtered_files;
 	}
 
+	// Function takes in directory and returns the list of files in that
+	// directory.
+	private String[] getSyncDirFiles(File folder) {
 
+		File[] listOfFiles = folder.listFiles();
+		Vector<String> files = new Vector<String>();
+		String[] listedfiles;
+		for (int i = 0; i < listOfFiles.length; i++) {
 
-	// Function takes in directory and returns the list of files in that directory.
-	private String[] getSyncDirFiles(File folder){
-
-		File[] listOfFiles = folder.listFiles(); 
-		Vector<String> files = new Vector<String>() ;
-		String [] listedfiles;
-		for (int i = 0; i < listOfFiles.length; i++){
-
-			if (listOfFiles[i].isFile()){
+			if (listOfFiles[i].isFile()) {
 				files.add(listOfFiles[i].getName());
 			}
 		}
 
 		listedfiles = new String[files.size()];
-		for(int i=0;i<files.size();i++){
-			listedfiles[i]=files.get(i);
+		for (int i = 0; i < files.size(); i++) {
+			listedfiles[i] = files.get(i);
 		}
-
 
 		return listedfiles;
 	}
 
-	private void send_over_socket(String flag, String[]control_data) throws IOException{
+	private void send_over_socket(String flag, String[] control_data)
+			throws IOException {
 		Packet pkt = new Packet();
 		pkt.setflag(flag);
 		pkt.setControlData(control_data);
-		ObjectOutputStream  oos = new 
-				ObjectOutputStream(this.socket.getOutputStream());
+		ObjectOutputStream oos = new ObjectOutputStream(
+				this.socket.getOutputStream());
 		oos.writeObject(pkt);
 
 	}
 
-	private Packet read_from_socket() throws IOException, ClassNotFoundException{
+	private Packet read_from_socket() throws IOException,
+			ClassNotFoundException {
 
-		ObjectInputStream ois = 
-				new ObjectInputStream(this.socket.getInputStream());
+		ObjectInputStream ois = new ObjectInputStream(
+				this.socket.getInputStream());
 
-		Packet pkt = (Packet)ois.readObject();
+		Packet pkt = (Packet) ois.readObject();
 		return pkt;
 
 	}
--- src/com/filesync/peer/PeerInstance.java
+++ src/com/filesync/peer/PeerInstance.java
@@ -14,165 +14,169 @@ import com.filesync.shareddir.SharedFolderDetails;
 
 public class PeerInstance {
 
-
-
 	public static void safePrintln(String s) {
 		synchronized (System.out) {
-			System.out.println("Main Thread: "+s);
+			System.out.println("Main Thread: " + s);
 		}
 	}
 
-
 	/**
 	 * @param args
-	 * @throws IOException 
-	 * @throws NumberFormatException 
+	 * @throws IOException
+	 * @throws NumberFormatException
 	 */
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws NumberFormatException,
+			IOException {
 
+		// Making some changes
 		safePrintln("Initializing Peer Node instance....");
 
-		//1. Initialize instance of Download Connections Manager and Upload Connections Manager
+		// 1. Initialize instance of Download Connections Manager and Upload
+		// Connections Manager
 		DownloadManager dm = new DownloadManager();
 		UploadManager um = new UploadManager();
 
-		//2. Get the listening thread running in well known port.	
-		PeerServerSocket ps = new PeerServerSocket(um,dm);
+		// 2. Get the listening thread running in well known port.
+		PeerServerSocket ps = new PeerServerSocket(um, dm);
 		Thread listening_thread = new Thread(ps);
 		listening_thread.setName("Peer Listening Socket Thread");
 		listening_thread.start();
 
 		safePrintln("Listening socket thread is up and running!");
 
-
-		//2.1 If the synchronizing folder does not exist, then create it.
+		// 2.1 If the synchronizing folder does not exist, then create it.
 		File syncdir = new File(SharedFolderDetails.directorypath);
-		if (!syncdir.exists()){
-			boolean result = syncdir.mkdir();  
-			if(result){    
-				safePrintln("Synchronizing Directory : "+ SharedFolderDetails.directorypath+" created.");  
+		if (!syncdir.exists()) {
+			boolean result = syncdir.mkdir();
+			if (result) {
+				safePrintln("Synchronizing Directory : "
+						+ SharedFolderDetails.directorypath + " created.");
 			}
 		}
 
-		//2.2 If the logging folder does not exist, then create it.
+		// 2.2 If the logging folder does not exist, then create it.
 		File logdir = new File(SharedFolderDetails.logdir);
-		if (!logdir.exists()){
-			boolean result = logdir.mkdir();  
-			if(result){    
-				safePrintln("Log Directory : "+ SharedFolderDetails.logdir+" created.");  
+		if (!logdir.exists()) {
+			boolean result = logdir.mkdir();
+			if (result) {
+				safePrintln("Log Directory : " + SharedFolderDetails.logdir
+						+ " created.");
 			}
 		}
 
-		//3. Main Menu display
+		// 3. Main Menu display
 		int choice;
-		do{
+		do {
 
 			safePrintln("********** NODE CONTROL *************");
 			safePrintln("1. Connect to a remote node ");
 			safePrintln("2. View all active connections");
 			safePrintln("3. Close an existing connection");
 			safePrintln("*************************************");
-			safePrintln ("Please enter your option (1-3): ");
+			safePrintln("Please enter your option (1-3): ");
 
-			//4. Get user input
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+			// 4. Get user input
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));
 			choice = Integer.parseInt(br.readLine());
 
-			if(choice < 1 || choice > 3){
-				safePrintln ("Please enter valid input between 1- 3");
+			if (choice < 1 || choice > 3) {
+				safePrintln("Please enter valid input between 1- 3");
 				continue;
 			}
 
-
-			//5. Process the choice which was chosen by the user.
-			if (choice ==1 ){
-				//	5.1 Connect to a node option.
+			// 5. Process the choice which was chosen by the user.
+			if (choice == 1) {
+				// 5.1 Connect to a node option.
 
 				InetAddress ip_address;
 				String remoteip;
 
-				do{
+				do {
 					safePrintln("********* Connect to a node ************");
 					safePrintln("Enter the IP Address of the node in string format separated by '.' For eg: 192.168.5.3");
-
 
 					try {
 						remoteip = br.readLine();
 						ip_address = InetAddress.getByName(remoteip);
 						safePrintln("Checking if a node with that IP address is alive in the network ");
-						if(ip_address.isReachable(2000)){
+						if (ip_address.isReachable(2000)) {
 							safePrintln("Node found!");
-							// Check if either an outgoing or incoming connection already exists with this ip address.
-							if(dm.checkDownloadConn(remoteip)==true || um.checkUploadConn(remoteip)==true){
+							// Check if either an outgoing or incoming
+							// connection already exists with this ip address.
+							if (dm.checkDownloadConn(remoteip) == true
+									|| um.checkUploadConn(remoteip) == true) {
 								safePrintln("Existing connection already exists with that node. Please enter a different node.");
 								continue;
 							}
 							break;
-						}
-						else{
+						} else {
 							safePrintln("Could not confirm presence of Host in network. Please ensure you have entered correct IP address of remote host.");
 						}
 					} catch (UnknownHostException e) {
 						safePrintln("Please enter valid input for IP. Caught unknownhostexception.");
-					}
-					catch(IOException ie){
+					} catch (IOException ie) {
 						safePrintln("Caught IO Exception while trying to reach the node.");
 					}
 
-				}while(true);
+				} while (true);
 
-				connectToNode(remoteip,dm);
-			}else if (choice==2){
+				connectToNode(remoteip, dm);
+			} else if (choice == 2) {
 
 				print_all_download_connections(dm);
 				print_all_upload_connections(um);
 
-			}else if(choice == 3){
+			} else if (choice == 3) {
 
 				boolean conn_check = false;
-				do{
+				do {
 
 					safePrintln("********* Close an existing connection ************");
 					safePrintln("Enter the IP Address of the remote node whose connection needs to be closed in string format separated by '.' For eg: 192.168.5.3");
 					String remoteip;
 					remoteip = br.readLine();
-					safePrintln("Checking if a download/upload connection exists with this node at "+remoteip);
+					safePrintln("Checking if a download/upload connection exists with this node at "
+							+ remoteip);
 
-					if(dm.checkDownloadConn(remoteip) == false && um.checkUploadConn(remoteip)==false){
+					if (dm.checkDownloadConn(remoteip) == false
+							&& um.checkUploadConn(remoteip) == false) {
 
 						safePrintln("\n No download/upload connection exists currently with this node. Please enter a different Node.");
 						continue;
-					}
-					else {
-						if(dm.checkDownloadConn(remoteip)==true){
+					} else {
+						if (dm.checkDownloadConn(remoteip) == true) {
 
 							safePrintln("\n A download connection exists with the node. Closing the download connection now.");
 							dm.close_download_connection(remoteip);
 							conn_check = true;
-						} if(um.checkUploadConn(remoteip)==true){
+						}
+						if (um.checkUploadConn(remoteip) == true) {
 							safePrintln("\n An upload connection exists with the node. Closing the uploading connection now.");
 							um.close_upload_connection(remoteip);
 							conn_check = true;
 						}
 					}
-				}while(conn_check==false);
+				} while (conn_check == false);
 			}
 			System.out.println("\n\n\n\n\n");
-		}while(true);
+		} while (true);
 
-	}	
+	}
 
 	// Function to print all active Download Connections.
-	public static void print_all_download_connections( DownloadManager dm){
+	public static void print_all_download_connections(DownloadManager dm) {
 
-		Map<String,PeerClient> active_download_connections = dm.getActiveDownloadConnections();
+		Map<String, PeerClient> active_download_connections = dm
+				.getActiveDownloadConnections();
 
 		safePrintln("********** DOWNLOAD CONNECTIONS    *****************");
-		int no_active_connections =active_download_connections.keySet().size();		
-		safePrintln("Totally "+no_active_connections+" download connections are active currently.");
-		if(no_active_connections>0){
-			for(String remoteip: active_download_connections.keySet()){
-				safePrintln("Connection downloading from "+remoteip);
+		int no_active_connections = active_download_connections.keySet().size();
+		safePrintln("Totally " + no_active_connections
+				+ " download connections are active currently.");
+		if (no_active_connections > 0) {
+			for (String remoteip : active_download_connections.keySet()) {
+				safePrintln("Connection downloading from " + remoteip);
 			}
 		}
 		safePrintln("*****************************************************");
@@ -180,16 +184,18 @@ public class PeerInstance {
 	}
 
 	// Function to print all active Upload Connections.
-	public static void print_all_upload_connections( UploadManager um){
+	public static void print_all_upload_connections(UploadManager um) {
 
-		Map<String,PeerServer> active_upload_connections = um.getActiveUploadConnections();
+		Map<String, PeerServer> active_upload_connections = um
+				.getActiveUploadConnections();
 
 		safePrintln("********** UPLOAD CONNECTIONS    *****************");
-		int no_active_connections =active_upload_connections.keySet().size();		
-		safePrintln("Totally "+no_active_connections+" upload connections are active currently.");
-		if(no_active_connections > 0){
-			for(String remoteip: active_upload_connections.keySet()){
-				safePrintln("Connection uploading to "+remoteip);
+		int no_active_connections = active_upload_connections.keySet().size();
+		safePrintln("Totally " + no_active_connections
+				+ " upload connections are active currently.");
+		if (no_active_connections > 0) {
+			for (String remoteip : active_upload_connections.keySet()) {
+				safePrintln("Connection uploading to " + remoteip);
 			}
 		}
 		safePrintln("*****************************************************");
@@ -197,13 +203,14 @@ public class PeerInstance {
 	}
 
 	// Function to make a connection to a new node.
-	public static void connectToNode(String remoteip, DownloadManager dm){
+	public static void connectToNode(String remoteip, DownloadManager dm) {
 
-		safePrintln("Connecting to the node at "+remoteip);		
-		PeerClient peerclient = new PeerClient(remoteip,dm.getActiveDownloadConnections());
+		safePrintln("Connecting to the node at " + remoteip);
+		PeerClient peerclient = new PeerClient(remoteip,
+				dm.getActiveDownloadConnections());
 		dm.add_download_connection(remoteip, peerclient);
 		Thread c = new Thread(peerclient);
-		c.setName("Client Thread to "+remoteip);
+		c.setName("Client Thread to " + remoteip);
 		c.start();
 
 	}
